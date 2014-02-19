6ac66fa68ea3b4916640cb96f0f63bb21b6b4016
yyang <git@iyyang.com>
2014-02-19 17:02:23 +0000

    Further revised access control.

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ var config = { "db": {
   'flavor': "regular",
   'debug': false,
   'accessControl': {
-    'allowMethods': ['GET', 'PUT', 'POST', 'DELETE']
+    "allowOrigin": "*",
+    "allowMethods": "GET,POST,PUT,DELETE,HEAD,OPTIONS"
   }
 };
 
